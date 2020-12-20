0c815df1fd9111afc47d2d0e2b30201c585f264f
Andrew Libby <alibby@andylibby.org>
2020-12-20 14:01:13 -0500

    Deserialize directly into geo::Coordinate

--- src/main.rs
+++ src/main.rs
@@ -8,47 +8,42 @@ use geo::LineString;
 
 use std::env;
 use std::fs::File;
-use serde::Deserialize;
 use wkt::ToWkt;
-
-#[derive(Debug, Deserialize)]
-struct Coord {
-    x: f32,
-    y: f32,
-}
 
 fn make_wkt_string(ls : LineString<f32>) -> String {
     let g = Geometry::from(ls);
     format!("{}", g.to_wkt().items[0])
 }
 
-fn read_points(filename: &str) -> std::io::Result<LineString<f32>> {
-    let f = File::open(filename)?;
+fn read_points(f: File) -> LineString<f32> {
+    let coordinator = |result| -> Option<Coordinate<f32>> {
+        match result {
+            Ok(coord) => Some(coord),
+            _ => None,
+        }
+    };
+
     let mut rdr = csv::ReaderBuilder::new()
         .has_headers(false)
         .trim(csv::Trim::All)
         .from_reader(f);
 
-    let points = rdr.deserialize()
-        .map(|result| -> Option<Coord> {
-            match result {
-                Ok(coord) => Some(coord),
-                _ => None,
-            }
-        })
+    let points = rdr
+        .deserialize()
+        .map(coordinator)
         .filter(|option| option.is_some())
         .map(|option| option.unwrap())
-        .map(|coord| -> Coordinate<f32> { Coordinate { x: coord.x, y: coord.y } })
         .collect();
 
-    let ls = LineString(points);
-    Ok(ls)
+    LineString(points)
 }
 
 fn main() -> std::io::Result<()> {
     env::args().skip(1).for_each(|filename: String| {
-        let ls = read_points(&filename[..]).unwrap();
-        println!("{}", make_wkt_string(ls));
+        match File::open(&filename[..]) {
+            Ok(f) => println!("{}", make_wkt_string(read_points(f))),
+            Err(e) => eprintln!("Error '{}': {}", filename, e),
+        };
     });
 
     Ok(())
